9612f6608cbc029d75254c34f50bd02f50fec44a
Cyn-Thea <fadzai@net.chalmers.se>
2023-01-27 11:21:03 +0100

    update

--- Exercise5.c
+++ Exercise5.c
@@ -1,25 +1,40 @@
 #include <stdio.h> //header file
 #include<stdlib.h> //stdlib file is included because rand() function is used to generate the table of random numbers
+#include <time.h>
+
 #define MAX 100 // Defines the maximum number of the values in the table 
 #define MAXNUMBER 20 // Defines the maximum value of random numbers 
-int table[MAX]; //defining "table" array of integers with MAX(100) of spaces
-int frequency[MAXNUMBER]; //defining an "frequency" array with MAXNUMBER(20) spaces
+
+int table[MAX], n ; //defining "table" array of integers with MAX(100) of spaces
+int frequency[MAXNUMBER];  //defining an "frequency" array with MAXNUMBER(20) spaces
+
 // ------ Function declarations   ----------  
 
 // This function generates a set of random numbers 
 // and fills the table with these numbers 
-void create_random()
-{
 
-    int i;
-    for (i = 0; i < MAX; i++) { //for loop to create the table of random numbers
-        table[i] = rand() % MAXNUMBER; //using the rand() in-built function to auto-generate the numbers
-    }
-    printf("Array of random integers: "); //printing the array of numbers created 
-    for (i = 0; i < MAX; i++) {
-        printf("%d ", table[i]);
-    }
-    printf("\n"); //printing new line 
+void create_random(int *tab );
+
+// This function takes the *tab of random numbers
+// and creates a table with the frequency counts for these numbers 
+void count_frequency(int *tab, int *freq );
+
+// This function takes the frequency count table
+// and draws a histogram of the values in that frequency table
+void draw_histogram(int *freq );
+
+// ------ Function definitions ----------
+
+// Fills the table with randomly generated numbers 
+
+void create_random(int *tab){
+// Sets the starting seed based on time for each iteration
+ srand (time(NULL) );
+    for(int i= 0; i< MAX; i++){ // Loop to generate 100 random numbers
+       int random = rand ()% MAXNUMBER; // random number should be between 0 - 19
+       tab[i] = random; // Assign the generated number to tab
+       printf("%d\n", random);
+    }    
 }
 
 // This function takes the table of random numbers 
@@ -51,18 +66,15 @@ void draw_histogram(int* freq) {
     }
 }
 
-
-int main() {
-
-    
 // ------ Main   -------------------------- 
 
 // The main entry point for the program 
-    
 
+int main() {  
+    
     int* tab = table; //defining the pointer tab to table
     int* freq = frequency; //defining the pointer freq to frequency
-    create_random(); //function call to generate the table of random number
+    create_random(table);//function call to generate the table of random number
     count_frequency(tab, freq); //function call to count the frequency 
     draw_histogram(freq);
     return 0;
